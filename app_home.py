0333bdbba80d9cc4eb4b01d562e05730f5819ad5
Yeomdongsu <duaehdtn9@naver.com>
2023-11-26 17:27:14 +0900

    수정

--- app_home.py
+++ app_home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def app_run_home() :
-    st.header("데이터를 이용한 암 예측하기")
+    st.header("데이터를 이용한 암 예측하기!")
     st.markdown("\n")
     st.markdown("\n")
     st.info("10,000명의 의사 환자로 구성되며, 각 환자는 성별, 연령, 흡연, 피로, 알레르기 등\n\n5가지 고유한 매개변수와 암의 유무를 나타내는 이진 지표를 특징으로 합니다.")
